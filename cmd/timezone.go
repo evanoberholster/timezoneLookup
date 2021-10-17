9319a5328a7186efdb40f4cb27dd3c115306ec86
Tamás Gulácsi <tamas@gulacsi.eu>
2021-10-17 10:26:53 +0200

    cmd/timezone.go: Download 2020d if not exist

--- cmd/timezone.go
+++ cmd/timezone.go
@@ -5,9 +5,14 @@
 package main
 
 import (
+	"archive/zip"
+	"bytes"
 	"errors"
 	"flag"
+	"io"
 	"log"
+	"net/http"
+	"os"
 
 	timezone "github.com/evanoberholster/timezoneLookup"
 )
@@ -17,6 +22,7 @@ var (
 	jsonFilename = flag.String("json", "combined-with-oceans.json", "GEOJSON Filename")
 	dbFilename   = flag.String("db", "timezone", "Destination database filename")
 	storageType  = flag.String("type", "boltdb", "Storage: boltdb or memory")
+	jsonURL      = flag.String("json-url", "https://github.com/evansiroky/timezone-boundary-builder/releases/download/2020d/timezones-with-oceans.geojson.zip", "Download GeoJSON file from here if not exist")
 	encoding     = flag.String("encoding", "msgpack", "BoltDB encoding type: json or msgpack")
 )
 
@@ -43,6 +49,38 @@ func Main() error {
 
 	if *jsonFilename == "" {
 		return errors.New("\"-json\" No GeoJSON source file specified")
+	}
+	if _, err := os.Stat(*jsonFilename); err != nil && os.IsNotExist(err) {
+		log.Println("Downloading " + *jsonURL)
+		resp, err := http.Get(*jsonURL)
+		if err != nil {
+			return err
+		}
+		var buf bytes.Buffer
+		_, err = io.Copy(&buf, resp.Body)
+		resp.Body.Close()
+		if err != nil {
+			return err
+		}
+		zr, err := zip.NewReader(bytes.NewReader(buf.Bytes()), int64(buf.Len()))
+		if err != nil {
+			return err
+		}
+		sr, err := zr.Open("combined-with-oceans.json")
+		if err != nil {
+			return err
+		}
+		fh, err := os.Create(*jsonFilename)
+		if err != nil {
+			return err
+		}
+		defer fh.Close()
+		if _, err = io.Copy(fh, sr); err != nil {
+			return err
+		}
+		if err := fh.Close(); err != nil {
+			return err
+		}
 	}
 	err := tz.CreateTimezones(*jsonFilename)
 	if err != nil {
