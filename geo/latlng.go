599df07359138b7c2346d8be482f052486a53469
Evan Oberholster <eroberholster@gmail.com>
2022-01-16 13:31:47 -0500

    add geo library

---
+++ geo/latlng.go
@@ -0,0 +1,68 @@
+// Copyright 2022 Evan Oberholster. All rights reserved.
+// Use of this source code is governed by an MIT-style
+// license that can be found in the LICENSE file.
+
+package geo
+
+// LatLng represents Latitude and Longitude in degree format
+type LatLng struct {
+	Lat, Lng float32
+}
+
+func (ll LatLng) toFloat32() [2]float32 {
+	return [2]float32{ll.Lat, ll.Lng}
+}
+
+// NewLatLng returns a new LatLng with the given latitude and longitude
+func NewLatLng(latitude, longitude float64) LatLng {
+	return LatLng{float32(latitude), float32(longitude)}
+}
+
+// Search LatLng
+func (tr *RTree) SearchLatLng(ll LatLng, iter func(min, max LatLng, value interface{}) bool) {
+	tr.searchLatLng(rect{min: ll.toFloat32(), max: ll.toFloat32()}, iter)
+}
+
+func (tr *RTree) searchLatLng(
+	target rect,
+	iter func(min, max LatLng, value interface{}) bool,
+) {
+	if tr.root.data == nil {
+		return
+	}
+	if target.intersects(&tr.root) {
+		tr.root.searchLatLng(target, tr.height, iter)
+	}
+}
+
+func (r *rect) searchLatLng(
+	target rect, height int,
+	iter func(min, max LatLng, value interface{}) bool,
+) bool {
+	n := r.data.(*node)
+	if height == 0 {
+		for i := 0; i < n.count; i++ {
+			if target.intersects(&n.rects[i]) {
+				if !iter(LatLng{n.rects[i].min[0], n.rects[i].min[1]}, LatLng{n.rects[i].max[0], n.rects[i].max[1]}, n.rects[i].data) {
+					return false
+				}
+			}
+		}
+	} else {
+		for i := 0; i < n.count; i++ {
+			if target.intersects(&n.rects[i]) {
+				if !n.rects[i].searchLatLng(target, height-1, iter) {
+					return false
+				}
+			}
+		}
+	}
+	return true
+}
+
+// InsertPolygon data into tree
+func (tr *RTree) InsertPolygon(p Polygon, value interface{}) {
+	var item rect
+	fit(p.min.toFloat32(), p.max.toFloat32(), value, &item)
+	tr.insert(&item)
+}
---
+++ geo/polygon.go
@@ -0,0 +1,158 @@
+// Copyright 2022 Evan Oberholster. All rights reserved.
+// Use of this source code is governed by an MIT-style
+// license that can be found in the LICENSE file.
+
+package geo
+
+import (
+	"reflect"
+	"unsafe"
+)
+
+const (
+	minLatitude  = -90
+	maxLatitude  = 90
+	minLongitude = -180
+	maxLongitude = 180
+)
+
+// Polygon represents a closed Polygon of vertices when
+// the first and last vertices are equal.
+type Polygon struct {
+	min, max LatLng   // min and man LatLng
+	v        []LatLng // Vertices
+}
+
+// NewPolygon returns a new empty Polygon
+func NewPolygon() Polygon {
+	return Polygon{
+		max: NewLatLng(minLatitude, minLongitude),
+		min: NewLatLng(maxLatitude, maxLongitude),
+	}
+}
+
+// NewPolygonFromVertices returns a new Polygon with the LatLng vertices.
+// updates the polygon's boundingbox.
+func NewPolygonFromVertices(s []LatLng) Polygon {
+	p := NewPolygon()
+	p.v = s
+	p.UpdateBoundingBox()
+	return p
+}
+
+// Valid returns true of LatLng is with the bounds of minLatitude/maxLatitude and minLongitude/maxLongitude
+func (ll LatLng) Valid() bool {
+	return (ll.Lat >= minLatitude || ll.Lat <= maxLatitude) && (ll.Lng >= minLongitude || ll.Lng <= maxLongitude)
+}
+
+func NewPolygonFromBytes(b []byte) Polygon {
+	p := NewPolygon()
+	p.v = toLatLngSlice(b)
+	for _, v := range p.v {
+		p.updateBounds(v)
+	}
+	return p
+}
+
+// updateBounds updates the max and min limits of the boundingBox.
+func (p *Polygon) updateBounds(ll LatLng) {
+	if ll.Valid() {
+		if p.max.Lat < ll.Lat {
+			p.max.Lat = ll.Lat
+		}
+		if p.max.Lng < ll.Lng {
+			p.max.Lng = ll.Lng
+		}
+		if p.min.Lat > ll.Lat {
+			p.min.Lat = ll.Lat
+		}
+		if p.min.Lng > ll.Lng {
+			p.min.Lng = ll.Lng
+		}
+	}
+}
+
+// Length retuns the number of veritices in the Polygon
+func (p *Polygon) Length() int {
+	return len(p.v)
+}
+
+// Max returns the bottom-left coordinate of the Polygon.
+// Correspoinding to the minimum latitide and longitude values contained.
+func (p *Polygon) Min() LatLng {
+	return p.min
+}
+
+// Max returns the top-right coordinate of the Polygon.
+// Correspoinding to the maximum latitude and longitude values contained.
+func (p *Polygon) Max() LatLng {
+	return p.max
+}
+
+// Add adds a Latitude and Longitude in degrees to the Polygon.
+// Maximum and minimum latitude is -90 and +90 respectively.
+// Maximum and minimum longitude is -180 and +180 respectively.
+func (p *Polygon) Add(latitude, longitude float64) {
+	p.AddVertex(LatLng{float32(latitude), float32(longitude)})
+}
+
+// AddVertex adds a LatLng Vertex to the Polygon. Updates polygon bounds with new Vertex.
+func (p *Polygon) AddVertex(ll LatLng) {
+	if ll.Valid() {
+		p.v = append(p.v, ll)
+		p.updateBounds(ll)
+	}
+}
+
+// UpdateBoundingBox updates the max and min limits of the boundingBox using the contained ploygon vertices.
+func (p *Polygon) UpdateBoundingBox() {
+	for _, v := range p.v {
+		p.updateBounds(v)
+	}
+}
+
+func (p *Polygon) ContainsLatLng(query LatLng) bool {
+	if len(p.v) < 3 {
+		return false
+	}
+	in := rayIntersectsSegment(query, p.v[len(p.v)-1], p.v[0])
+	for i := 1; i < len(p.v); i++ {
+		if rayIntersectsSegment(query, p.v[i-1], p.v[i]) {
+			in = !in
+		}
+	}
+	return in
+}
+
+func rayIntersectsSegment(p, a, b LatLng) bool {
+	return (a.Lng > p.Lng) != (b.Lng > p.Lng) &&
+		p.Lat < (b.Lat-a.Lat)*(p.Lng-a.Lng)/(b.Lng-a.Lng)+a.Lat
+}
+
+// reference: https://go101.org/article/unsafe.html
+func toByteSlice(b []LatLng) []byte {
+	var bs []byte
+	hdr := (*reflect.SliceHeader)(unsafe.Pointer(&bs))
+	hdr.Len = len(b) * 8
+	hdr.Cap = hdr.Len
+	hdr.Data = uintptr(unsafe.Pointer(&b[0]))
+	return bs
+}
+
+// reference: https://go101.org/article/unsafe.html
+func toLatLngSlice(b []byte) (result []LatLng) {
+	var lls []LatLng
+	hdr := (*reflect.SliceHeader)(unsafe.Pointer(&lls))
+	hdr.Len = len(b) / 8
+	hdr.Cap = hdr.Len
+	hdr.Data = uintptr(unsafe.Pointer(&b[0]))
+	return lls
+}
+
+func (p Polygon) ToByteSlice() []byte {
+	return toByteSlice(p.v)
+}
+
+func (p *Polygon) FromByteSlice(src []byte) {
+	p.v = toLatLngSlice(src)
+}
